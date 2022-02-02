7955c38dc6028d512a8285659a53cae8bcedc111
contateobrito <contateobrito@usp.br>
2022-02-02 00:23:38 -0300

    Novos exercicios

---
+++ Python/Class_04.py
@@ -0,0 +1,12 @@
+class Estudantes():
+    def __init__(self, nome, idade, nota):
+        self.nome = nome
+        self.idade = idade
+        self.nota = nota
+
+Estudante1 = Estudantes('Danilo', 35, 7.5)
+
+print(Estudante1.nome)
+print(Estudante1.idade)
+print(Estudante1.nota)
+
